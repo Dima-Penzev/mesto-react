59dfee996cdbce16f3f6f2f963fb42d493a50ed4
Dima <D130188@yandex.ru>
2023-04-20 23:37:16 +0400

    added Api

---
+++ src/components/Api.js
@@ -0,0 +1,76 @@
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _getResponseData(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, { headers: this._headers }).then(
+      (res) => this._getResponseData(res)
+    );
+  }
+
+  addNewCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    }).then((res) => this._getResponseData(res));
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => {
+      this._getResponseData(res);
+    });
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, { headers: this._headers }).then(
+      (res) => this._getResponseData(res)
+    );
+  }
+
+  setUserInfo({ username, activity }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: username,
+        about: activity,
+      }),
+    }).then((res) => this._getResponseData(res));
+  }
+
+  setUserPhoto(link) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: link,
+      }),
+    }).then((res) => this._getResponseData(res));
+  }
+
+  toggleLikeState(cardId, isLiked) {
+    const method = isLiked ? "DELETE" : "PUT";
+
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method,
+      headers: this._headers,
+    }).then((res) => this._getResponseData(res));
+  }
+}
